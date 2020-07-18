88d5d08c2f9be099706f6b2e7fd69b69ec4ab092
Ismayel <ismayelhossen123@gmail.com>
2020-07-18 16:42:13 +0600

    Create AddToenv.blade.php file and add client id,client secret id

---
+++ AddToenv.blade.php
@@ -0,0 +1,12 @@
+<!--from https://github.com/settings/applications/new.here create new api and generate client_id,client_secret -->
+client_id=d3b6f24d28fcb434eadc
+client_secret=4b7ba762d4f7e578efa49d3e915d6076616cb4cd
+<!-- github end -->
+<!-- from https://console.developers.google.com/apis/ -->
+client_idg=916124505035-mqq929nd6j2d3ss3d2e2sskgk4j9lkcf.apps.googleusercontent.com
+client_secretg=-ScNVzCdXvln1_LWqTb-1hGm
+<!-- end google -->
+<!-- from https://developers.facebook.com/apps -->
+client_idf=2947118848730212
+client_secretf=e4a11a8d4a7db47353e341b1b3f8a3fb
+<!-- end -->
